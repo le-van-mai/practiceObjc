e4f6370804cc5aae037fd159fd91998c7bfb7ce5
MacOS <macos@MacBook-Pro-cua-MacOS.local>
2023-06-03 08:39:52 +0700

    first commit

---
+++ testmmo/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  testmmo
+//
+//  Created by MacOS on 6/3/23.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+     
+//        2013-09-22 21:20:09.845 Inheritance[349:303] Name: Raj
+//        2013-09-22 21:20:09.846 Inheritance[349:303] Age: 5
+//        2013-09-22 21:20:09.846 Inheritance[349:303] Education: MBA
+        // testPerson {} input: Mailee 18 
+       // Person *testPerson = [[Person alloc] initWithName:@"MaiLee" andAge:18];
+       // [testPerson print];
+    }
+    return 0;
+}
